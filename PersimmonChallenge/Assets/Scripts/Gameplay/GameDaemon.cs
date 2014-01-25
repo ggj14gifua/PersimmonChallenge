f3beeefaf84b4aeb09976cae76ee4f911657a68d
urahimono <urahimono@gmail.com>
2014-01-25 21:01:14 +0900

    ライトを時間で回転させる。

--- PersimmonChallenge/Assets/Scripts/Gameplay/GameDaemon.cs
+++ PersimmonChallenge/Assets/Scripts/Gameplay/GameDaemon.cs
@@ -7,8 +7,8 @@ namespace Gameplay
 {
     class GameDaemon : GameSystem.SingletonMonoBehaviour< GameDaemon >
     {
-
-        [SerializeField] private Light      m_mainLight  = null;
+        [SerializeField] private Light      m_mainLight     = null;
+        [SerializeField] private float      m_rotSpeed      = 1.0f;
 
         public Light MainLight { get { return m_mainLight; } set { m_mainLight = value; } }
 
@@ -32,6 +32,9 @@ namespace Gameplay
 
 	    void Update()
 	    {
+            Vector3 lightRotAngles = MainLight.transform.localRotation.eulerAngles;
+            MainLight.transform.rotation = Quaternion.Euler(lightRotAngles.x, lightRotAngles.y + Time.deltaTime * m_rotSpeed, lightRotAngles.z);
+
 
 	    }
 //------------------------------------------------------------------------
