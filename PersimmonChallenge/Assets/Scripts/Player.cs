71747662dd791d4b08c3657672a6c06ee00ec498
SatozakiMasato <kyozyu_masa@yahoo.co.jp>
2014-01-26 01:24:07 +0900

    ボールの削除と再出現を追加

--- PersimmonChallenge/Assets/Scripts/Player.cs
+++ PersimmonChallenge/Assets/Scripts/Player.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-	float acceleration = 0.01f;
+	int life = 3;
+	int returnInterval = 0;
+	float acceleration = 0.02f;
+	bool stateFlag = true;
+	bool gameOverFlag = false;
+	Vector3 startPosition = new Vector3( -2.8f, 1.4f, 2.7f );
 
 	// Use this for initialization
 	void Start ()
 	{
-
 	}
 	
 	// Update is called once per frame
@@ -48,6 +52,40 @@ public class Player : MonoBehaviour
 			float z = rigidbody.velocity.z + acceleration;
 			
 			rigidbody.velocity = new Vector3(x, y, z);
+		}
+
+		if ( stateFlag == false )
+		{
+			++returnInterval;
+
+			if ( returnInterval == 30 && gameOverFlag == false )
+			{
+				transform.position = startPosition;
+				returnInterval = 0;
+				stateFlag = true;
+				renderer.enabled = true;
+			}
+		}
+	}
+
+	// ボーダーラインとの衝突判定
+	void OnTriggerEnter( Collider other )
+	{
+		if ( other.name == "Borderline" )
+		{
+			// true：描画・false：描画しない
+			renderer.enabled = false;
+
+			// 残機を減らす
+			life -= 1;
+
+			if ( life == 0 )
+			{
+				Scene.canNextScene = true;
+				gameOverFlag = true;
+			}
+
+			stateFlag = false;
 		}
 	}
 }
