03ce8a370c44481c5ae09f5815a1e1f6cda1c4f7
urahimono <urahimono@gmail.com>
2014-01-25 19:54:22 +0900

    影の当たり判定。

---
+++ PersimmonChallenge/Assets/Scripts/Gameplay/GameDaemon.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using GameSystem;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gameplay
+{
+    class GameDaemon : GameSystem.SingletonMonoBehaviour< GameDaemon >
+    {
+
+        [SerializeField] private Light      m_mainLight  = null;
+
+        public Light MainLight { get { return m_mainLight; } set { m_mainLight = value; } }
+
+//////////////////////////////////////////////////////////////////////////
+// 
+// BehaviourBase
+// 
+//////////////////////////////////////////////////////////////////////////
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+//------------------------------------------------------------------------
+
+	    void Start()
+	    {
+
+        }
+//------------------------------------------------------------------------
+
+	    void Update()
+	    {
+
+	    }
+//------------------------------------------------------------------------
+
+    } // class GameDaemon
+
+} // namespace Gameplay
