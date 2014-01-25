9c99025a238c8a84abec6628f634882bd7666844
Ryota Morita <ryota.morita.3.8@gmail.com>
2014-01-25 17:34:03 +0900

    gameシーンを追加

---
+++ PersimmonChallenge/Assets/Scripts/GameScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// 0: Mouse left button
+		// 1: Mouse right button
+		// 2: Mouse center button
+		if (Input.GetMouseButtonUp(0)) {
+			Scene.canNextScene = true;
+		}
+	}
+}
--- PersimmonChallenge/Assets/Scripts/Scene.cs
+++ PersimmonChallenge/Assets/Scripts/Scene.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class Scene : MonoBehaviour {
 
+	public string NextScene;
+	public static bool canNextScene = false;
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (canNextScene) {
+			Application.LoadLevel(NextScene);
+			canNextScene = false;
+		}
 	}
 }
