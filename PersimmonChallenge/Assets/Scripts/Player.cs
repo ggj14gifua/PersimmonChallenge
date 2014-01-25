b8297bef322246761920611192e0ebb560523ae8
SatozakiMasato <kyozyu_masa@yahoo.co.jp>
2014-01-25 19:57:41 +0900

    ボールを追加

---
+++ PersimmonChallenge/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	float acceleration = 0.01f;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if ( Input.GetKey( KeyCode.LeftArrow ) )
+		{
+			float x = rigidbody.velocity.x - acceleration;
+			float y = rigidbody.velocity.y;
+			float z = rigidbody.velocity.z;
+			
+			rigidbody.velocity = new Vector3(x, y, z);
+		}
+		
+		if ( Input.GetKey( KeyCode.RightArrow ) )
+		{
+			float x = rigidbody.velocity.x + acceleration;
+			float y = rigidbody.velocity.y;
+			float z = rigidbody.velocity.z;
+			
+			rigidbody.velocity = new Vector3(x, y, z);
+		}
+
+		if ( Input.GetKey( KeyCode.UpArrow ) )
+		{
+			float x = rigidbody.velocity.x;
+			float y = rigidbody.velocity.y;
+			float z = rigidbody.velocity.z + acceleration;
+			
+			rigidbody.velocity = new Vector3(x, y, z);
+		}
+		
+		if ( Input.GetKey( KeyCode.DownArrow ) )
+		{
+			float x = rigidbody.velocity.x;
+			float y = rigidbody.velocity.y;
+			float z = rigidbody.velocity.z - acceleration;
+			
+			rigidbody.velocity = new Vector3(x, y, z);
+		}
+	}
+}
