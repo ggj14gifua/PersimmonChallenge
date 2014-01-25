0caa8da8005a8c5100c87c480af0e00d6c34a9c1
urahimono <urahimono@gmail.com>
2014-01-25 17:40:50 +0900

    簡易シングルトンのスクリプト

---
+++ PersimmonChallenge/Assets/Scripts/GameSystem/Singleton.cs
@@ -0,0 +1,35 @@
+﻿
+namespace GameSystem
+{
+    public class Singleton< TMyClass> where TMyClass : class
+    {
+        private static TMyClass    s_instance = null;
+
+        protected Singleton()
+        {
+            
+        }
+//------------------------------------------------------------------------
+
+//////////////////////////////////////////////////////////////////////////
+// 
+// Property
+// 
+//////////////////////////////////////////////////////////////////////////
+
+        public static TMyClass Instance
+        {
+            get
+            {
+                if( s_instance == null )
+                {
+                    s_instance = System.Activator.CreateInstance( typeof( TMyClass ), true ) as TMyClass;
+                }
+                return s_instance;
+            }
+        }
+//------------------------------------------------------------------------
+       
+    } // class Singleton
+
+} // namespace GameSystem
---
+++ PersimmonChallenge/Assets/Scripts/GameSystem/SingletonMonoBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace GameSystem
+{
+    /// <summary>
+    /// MonoBehaviour用Singleton。
+    /// </summary>
+    class SingletonMonoBehaviour< TMyClass > : MonoBehaviour where TMyClass : MonoBehaviour
+    {
+        private static  TMyClass    s_instance  = null;
+
+
+//////////////////////////////////////////////////////////////////////////
+// 
+// Property
+// 
+//////////////////////////////////////////////////////////////////////////
+
+        public static TMyClass Instance
+        {
+            get
+            {
+                if( s_instance == null )
+                {
+                    s_instance = (TMyClass)FindObjectOfType( typeof(TMyClass) );
+                }
+
+                return s_instance;
+            }
+            private set
+            {
+                s_instance  = value;
+            }
+        }
+//------------------------------------------------------------------------
+
+//////////////////////////////////////////////////////////////////////////
+// 
+// BehaviourBase
+// 
+//////////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Unity:スクリプトのインスタンスがロードされたときに呼び出される。
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if( this != Instance )
+            {
+                Destroy( this );
+            }
+        }
+//------------------------------------------------------------------------
+
+    } // class SingletonMonoBehaviour
+
+} // namespace GameSystem
