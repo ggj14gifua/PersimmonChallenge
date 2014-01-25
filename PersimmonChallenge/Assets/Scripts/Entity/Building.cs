47e74b7501a09cf106358d8e58111c270eee405b
urahimono <urahimono@gmail.com>
2014-01-26 01:32:20 +0900

    太陽に左右されないオブジェクトを作成（向き固定）

--- PersimmonChallenge/Assets/Scripts/Entity/Building.cs
+++ PersimmonChallenge/Assets/Scripts/Entity/Building.cs
@@ -5,8 +5,20 @@ namespace Entity
 {
     class Building : MonoBehaviour
     {
+        private enum ELightType
+        {
+            Directional,
+            Spot,
+        }
+
         [SerializeField] private GameObject     m_buildingObject    = null;
         [SerializeField] private GameObject     m_shadowObject      = null;
+        [SerializeField] private ELightType     m_lightType         = ELightType.Directional;
+        [SerializeField] private float          m_spotAngle         = 0.0f;
+        [SerializeField] private float          m_spotLength        = 2.0f;
+        
+        private MeshRenderer                    m_buildingRenderer  = null;
+        private MeshRenderer                    m_shadowRenderer    = null;
 
         private const float BASEMESH_SIZE   = 10.0f;
         private const float BASEHALF_SIZE   = BASEMESH_SIZE * 0.5f;
@@ -21,7 +33,8 @@ namespace Entity
 
 	    void Awake()
         {
-
+            m_buildingRenderer  = m_buildingObject.GetComponent< MeshRenderer >();
+            m_shadowRenderer    = m_shadowObject.GetComponent< MeshRenderer >();
         }
 //------------------------------------------------------------------------
 
@@ -46,6 +59,17 @@ namespace Entity
 
         private void UpdateShadow()
         {
+            if (m_lightType == ELightType.Spot)
+            {
+                m_shadowRenderer.enabled        = true;
+                m_buildingRenderer.castShadows  = false;
+            }
+            else
+            {
+                m_shadowRenderer.enabled        = false;
+                m_buildingRenderer.castShadows  = true;
+            }
+
             UpdateShadowRotation();
             UpdateShadowScale();
             UpdateShadowPosition();
@@ -54,11 +78,36 @@ namespace Entity
 
         private void UpdateShadowRotation()
         {
-            Light   mainLight           = Gameplay.GameDaemon.Instance.MainLight;
+            if (m_lightType == ELightType.Spot)
+            {
+//                 GameObject  lightObj    = Gameplay.GameDaemon.Instance.FindNearSpotLight(gameObject);
+//                 Vector3     curPos      = transform.position;
+//                 Vector3     lightPos    = lightObj.transform.position;
+//                 Vector3     lightDir    = curPos - lightPos;
+//                 lightDir.y = 0.0f;
+//                 lightDir.Normalize();
+// 
+// 
+//                 float angle             = Vector3.Angle( Vector3.forward, lightDir );
+//                 if (curPos.x < lightPos.x)
+//                 {
+//                     angle = -angle;
+//                 }
 
-            Vector3 lightRotAngles      = mainLight.transform.localRotation.eulerAngles;
-            Vector3 shadowRotAngles     = m_shadowObject.transform.rotation.eulerAngles;
-            m_shadowObject.transform.rotation = Quaternion.Euler( shadowRotAngles.x, lightRotAngles.y, shadowRotAngles.z );
+
+                Vector3 shadowRotAngles = m_shadowObject.transform.rotation.eulerAngles;
+                m_shadowObject.transform.rotation = Quaternion.Euler(shadowRotAngles.x, /*angle*/m_spotAngle, shadowRotAngles.z);
+
+            }
+            else
+            {
+                Light   mainLight           = Gameplay.GameDaemon.Instance.MainLight;
+
+                Vector3 lightRotAngles      = mainLight.transform.localRotation.eulerAngles;
+                Vector3 shadowRotAngles     = m_shadowObject.transform.rotation.eulerAngles;
+                m_shadowObject.transform.rotation = Quaternion.Euler( shadowRotAngles.x, lightRotAngles.y, shadowRotAngles.z );
+            }
+            
         }
 //------------------------------------------------------------------------
 
@@ -73,33 +122,58 @@ namespace Entity
 
 
             }
-            Light   mainLight           = Gameplay.GameDaemon.Instance.MainLight;
 
-            Vector3 lightRotAngles      = mainLight.transform.localRotation.eulerAngles;
-            if( lightRotAngles.x >= 90.0f )
+            if (m_lightType == ELightType.Spot)
             {
-                return;
+                Vector3 scale   = m_shadowObject.transform.localScale;
+                scale.z         = m_spotLength / BASEMESH_SIZE;
+                m_shadowObject.transform.localScale = scale;
             }
+            else
+            {
+                Light   mainLight           = Gameplay.GameDaemon.Instance.MainLight;
 
-            float   rad     = lightRotAngles.x * Mathf.PI / 180;
-            float   tan     = Mathf.Tan( rad );
-            float   height  = m_buildingObject.transform.localScale.y * 2.0f;
-            float   length  = height / tan;
-            Vector3 scale   = m_shadowObject.transform.localScale;
-            scale.z         = length  / BASEMESH_SIZE * CTRL_SCLAE;
-            m_shadowObject.transform.localScale = scale;
+                Vector3 lightRotAngles      = mainLight.transform.localRotation.eulerAngles;
+                if( lightRotAngles.x >= 90.0f )
+                {
+                    return;
+                }
+
+                float   rad     = lightRotAngles.x * Mathf.PI / 180;
+                float   tan     = Mathf.Tan( rad );
+                float   height  = m_buildingObject.transform.localScale.y * 2.0f;
+                float   length  = height / tan;
+                Vector3 scale   = m_shadowObject.transform.localScale;
+                scale.z         = length  / BASEMESH_SIZE * CTRL_SCLAE;
+                m_shadowObject.transform.localScale = scale;
+            }
+            
         }
 //------------------------------------------------------------------------
 
         private void UpdateShadowPosition()
         {
-            Light mainLight         = Gameplay.GameDaemon.Instance.MainLight;
-            Vector3 lightRotAngles  = mainLight.transform.localRotation.eulerAngles;
+            Vector3 lightRotAngles  = Vector3.zero;
+            if (m_lightType == ELightType.Spot)
+            {
+                lightRotAngles      = m_shadowObject.transform.localRotation.eulerAngles;
+            }
+            else
+            {
+                Light mainLight     = Gameplay.GameDaemon.Instance.MainLight;
+                lightRotAngles      = mainLight.transform.localRotation.eulerAngles;
+            }
 
+            
             Quaternion  rot         = Quaternion.AngleAxis( lightRotAngles.y, Vector3.up );
             Vector3     direction   = rot * Vector3.forward;
             Vector3     pos         = m_buildingObject.transform.localPosition;
             pos.y                  -= m_buildingObject.transform.localScale.y;
+            if (m_lightType == ELightType.Spot)
+            {
+                pos.y              += m_buildingObject.transform.localScale.y * 0.5f;
+                pos.y              += 0.01f;
+            }
             pos                    += direction * BASEHALF_SIZE * m_shadowObject.transform.localScale.z;
 
             m_shadowObject.transform.localPosition = pos;
--- PersimmonChallenge/Assets/Scripts/Gameplay/GameDaemon.cs
+++ PersimmonChallenge/Assets/Scripts/Gameplay/GameDaemon.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 using GameSystem;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Gameplay
 {
     class GameDaemon : GameSystem.SingletonMonoBehaviour< GameDaemon >
     {
-        [SerializeField] private Light      m_mainLight     = null;
-        [SerializeField] private float      m_rotSpeed      = 1.0f;
+        [SerializeField] private Light          m_mainLight     = null;
+        [SerializeField] private GameObject     m_spotLights    = null;
+        [SerializeField] private float          m_rotSpeed      = 1.0f;
+
+        private List< GameObject >   m_spotLightList     = new List< GameObject >();
 
         public Light MainLight { get { return m_mainLight; } set { m_mainLight = value; } }
 
@@ -21,6 +23,12 @@ namespace Gameplay
         protected override void Awake()
         {
             base.Awake();
+
+//             foreach( Transform child in m_spotLights.transform )
+//             {
+//                 m_spotLightList.Add( child.gameObject );
+//             }
+            //m_spotLightList.AddRange( m_spotLights.GetComponentsInChildren< Light >() );
         }
 //------------------------------------------------------------------------
 
@@ -37,6 +45,41 @@ namespace Gameplay
 
 
 	    }
+//------------------------------------------------------------------------
+
+//////////////////////////////////////////////////////////////////////////
+// 
+// Public
+// 
+//////////////////////////////////////////////////////////////////////////
+
+        public GameObject FindNearSpotLight( GameObject i_object )
+        {
+            GameObject  nearLight     = null;
+            float       minLenSqr     = 0.0f;
+
+            Vector3 objPos  = i_object.transform.position;
+            foreach (var light in m_spotLightList)
+            {
+                Vector3 lightPos    = light.transform.position;
+                float   lenSqr      = ( objPos - lightPos ).sqrMagnitude;
+
+                if( nearLight == null )
+                {
+                    nearLight   = light;
+                    minLenSqr   = lenSqr;
+                    continue;
+                }
+
+                if( lenSqr < minLenSqr )
+                {
+                    nearLight   = light;
+                    minLenSqr   = lenSqr;
+                }
+            }
+
+            return nearLight;
+        }
 //------------------------------------------------------------------------
 
     } // class GameDaemon
